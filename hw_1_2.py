6900003e24b2a16613bdd3338a3df65f5b0b440c
liapal12 <47388870+liapal12@users.noreply.github.com>
2019-02-06 15:43:46 +0200

    Add files via upload

---
+++ hw_1_2.py
@@ -0,0 +1,18 @@
+
+# Задача №2
+# filename: hw_1_2.py
+
+# Запросить у пользователя число1 (число может быть любое).
+# Запросить у пользователя число2 (число может быть любое).
+# Вывести результат всех известных Вам допустимых операций над двумя числами.
+
+number_1 = input('Number #1 is: ')
+number_2 = input('Number #2 is: ')
+
+print(float(number_1) + float(number_2))
+print(float(number_1) - float(number_2))
+print(float(number_1) * float(number_2))
+print(float(number_1) / float(number_2))
+print(float(number_1) % float(number_2))
+print(float(number_1) ** float(number_2))
+print(float(number_1) // float(number_2))
